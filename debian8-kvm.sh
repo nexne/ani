a51292217dc1ee4efc0cb2f004382e5e37b0e5f7
nexne <nexne@unpak.ac.id>
2018-06-18 02:54:03 +0800

    Update debian8-kvm.sh

--- debian8-kvm.sh
+++ debian8-kvm.sh
@@ -442,6 +442,69 @@ service squid3 restart
 #service pptpd restart
 sysv-rc-conf rc.local on
 
+# download script
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/nexne/32n64/master/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/nexne/32n64/master/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/nexne/32n64/master/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/nexne/32n64/master/hapus.sh"
+wget -O login "https://raw.githubusercontent.com/nexne/32n64/master/user-login.sh"
+wget -O dropmon "https://raw.githubusercontent.com/nexne/32n64/master/dropmon.sh"
+wget -O user-expired.sh "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/freak/user-expired.sh"
+#wget -O userlimit.sh "https://raw.githubusercontent.com/suryadewa/fornesiavps/fns/limit.sh"
+wget -O member "https://raw.githubusercontent.com/nexne/32n64/master/user-list.sh"
+wget -O restart "https://raw.githubusercontent.com/nexne/32n64/master/resvis.sh"
+wget -O speedtest "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/speedtest_cli.py"
+wget -O bench-network "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/bench-network.sh"
+wget -O ps-mem "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/ps_mem.py"
+wget -O about "https://raw.githubusercontent.com/nexne/32n64/master/about.sh"
+wget -O delete "https://raw.githubusercontent.com/nexne/32n64/master/delete.sh"
+wget -O renew "https://raw.githubusercontent.com/nexne/32n64/master/renew.sh"
+wget -O kill "https://raw.githubusercontent.com/nexne/32n64/master/kill.sh"
+wget -O ban "https://raw.githubusercontent.com/nexne/32n64/master/ban.sh"
+wget -O unban "https://raw.githubusercontent.com/nexne/32n64/master/unban.sh"
+wget -O log "https://raw.githubusercontent.com/nexne/32n64/master/log.sh"
+wget -O rasakan "https://raw.githubusercontent.com/nexne/32n64/master/rasakan.sh"
+wget -O log1 "https://raw.githubusercontent.com/nexne/32n64/master/log1.sh"
+echo "0 0 * * * root /root/user-expired.sh" > /etc/cron.d/user-expired
+#echo "0 0 * * * root /usr/bin/expired" > /etc/cron.d/expired
+echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot
+echo "#* * * * * service dropbear restart" > /etc/cron.d/dropbear
+chmod +x menu
+chmod +x usernew
+chmod +x trial
+chmod +x hapus
+chmod +x login
+chmod +x dropmon
+chmod +x user-expired
+#chmod +x userlimit.sh
+chmod +x member
+chmod +x restart
+chmod +x speedtest
+chmod +x bench-network
+chmod +x ps-mem
+chmod +x about
+chmod +x delete
+chmod +x renew
+chmod +x user-expired.sh
+chmod +x kill
+chmod +x ban
+chmod +x unban
+chmod +x log
+chmod +x rasakan
+chmod +x log1
+cd
+echo "0 */12 * * * root /usr/bin/delete" >> /etc/crontab
+echo "#* * * * * root service dropbear restart" >> /etc/crontab
+echo "#0 */6 * * * root /usr/bin/restart" >> /etc/crontab
+#echo "#*/10 * * * * root service squid3 restart" >> /etc/crontab
+echo "#* * * * * root /usr/bin/kill" >> /etc/crontab
+#echo "#* * * * * root sleep 10; /usr/bin/kill" >> /etc/crontab
+echo "#0 */6 * * * root /usr/bin/ban" >> /etc/crontab
+echo "#* * * * * root /usr/bin/rasakan 2" >> /etc/crontab
+echo "0 3 * * * root /sbin/reboot" > /etc/cron.d/reboot
+service cron restart
+
 #clearing history
 history -c
 
