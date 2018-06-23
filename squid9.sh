334a35b04b1e14eb0bebf1f42c9c7be8db57f284
nexne <nexne@unpak.ac.id>
2018-06-24 02:58:57 +0800

    Update squid9.sh

--- squid9.sh
+++ squid9.sh
@@ -2,12 +2,12 @@
 # Created by https://www.hostingtermurah.net
 # Modified by 0123456
 # install wget and curl
-apt-get update;apt-get -y install wget curl;
+#apt-get update;apt-get -y install wget curl;
 
 #Requirement
 if [ ! -e /usr/bin/curl ]; then
-    apt-get -y update && apt-get -y upgrade
-	apt-get -y install curl
+    #apt-get -y update && apt-get -y upgrade
+	#apt-get -y install curl
 fi
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
@@ -44,8 +44,10 @@ http_access deny all
 http_port 8888
 http_port 8080
 http_port 8000
-http_port 80
 http_port 3128
+http_port 1080
+http_port 3130
+http_port 3000
 coredump_dir /var/spool/squid3
 refresh_pattern ^ftp: 1440 20% 10080
 refresh_pattern ^gopher: 1440 0% 1440
